8914e3649419e975b92d1f07eac85147edcd88d1
Cedric Lewe <0skillallluck@pm.me>
2026-05-07 12:07:05 +0200

    Fix Nix vendor hash

--- flake.nix
+++ flake.nix
@@ -113,7 +113,7 @@
           pname = "scanline";
           version = "0.4.0";
           src = pkgs.lib.cleanSource ./.;
-          vendorHash = "sha256-RQn9pK/JkzvpJTG9xADz91W40Ss19JEtZf+0N+zLUA=";
+          vendorHash = "sha256-RQn9pK/jfkzvpJTG9xADz91W40Ss19JEtZf+0N+zLUA=";
 
           ldflags = [
             "-X \"github.com/0skillallluck/scanline/app/dialogs/about.Commit=${
