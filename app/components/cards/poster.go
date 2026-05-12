6409962cf6756c9466867b616f20a51ff1b463c7
Cedric Lewe <0skillallluck@pm.me>
2026-05-12 11:22:55 +0200

    Use posterWithProgress only with progress

--- app/components/cards/poster.go
+++ app/components/cards/poster.go
@@ -29,11 +29,35 @@ func PosterCoverURL(src sources.Source) func(thumb string) string {
 }
 
 func poster[T any](title string, subTitle schwifty.Widgetable[T], coverURL string) schwifty.Button {
-	return posterWithProgress(title, subTitle, coverURL, 0)
+	image := posterPicture(coverURL).
+		CornerRadius(10).
+		Overflow(gtk.OverflowHiddenValue)
+
+	return posterButton(title, subTitle, image)
 }
 
 func posterWithProgress[T any](title string, subTitle schwifty.Widgetable[T], coverURL string, progress float64) schwifty.Button {
-	picture := Picture().
+	if progress <= 0 {
+		return poster(title, subTitle, coverURL)
+	}
+
+	progressBar := Box(gtk.OrientationHorizontalValue).
+		SizeRequest(int32(PosterWidth*progress), 4).
+		VAlign(gtk.AlignEndValue).
+		HAlign(gtk.AlignStartValue).
+		CSS("box { background-color: @accent_bg_color; }")
+
+	image := Bin().
+		Child(Overlay(posterPicture(coverURL)).AddOverlay(progressBar)).
+		SizeRequest(PosterWidth, PosterHeight).
+		CornerRadius(10).
+		Overflow(gtk.OverflowHiddenValue)
+
+	return posterButton(title, subTitle, image)
+}
+
+func posterPicture(coverURL string) schwifty.Picture {
+	return Picture().
 		SizeRequest(PosterWidth, PosterHeight).
 		FromPaintable(gdk.NewTextureFromResource("/dev/skillless/Scanline/icons/scalable/state/missing-album.svg")).
 		ConnectRealize(func(w gtk.Widget) {
@@ -41,24 +65,9 @@ func posterWithProgress[T any](title string, subTitle schwifty.Widgetable[T], co
 				imageutils.LoadIntoPictureScaled(coverURL, PosterWidth, PosterHeight, gtk.PictureNewFromInternalPtr(w.Ptr))
 			}
 		})
+}
 
-	var image any
-	if progress > 0 {
-		progressBar := Box(gtk.OrientationHorizontalValue).
-			SizeRequest(int32(PosterWidth*progress), 4).
-			VAlign(gtk.AlignEndValue).
-			HAlign(gtk.AlignStartValue).
-			CSS("box { background-color: @accent_bg_color; }")
-
-		image = Bin().
-			Child(Overlay(picture).AddOverlay(progressBar)).
-			SizeRequest(PosterWidth, PosterHeight).
-			CornerRadius(10).
-			Overflow(gtk.OverflowHiddenValue)
-	} else {
-		image = picture.CornerRadius(10).Overflow(gtk.OverflowHiddenValue)
-	}
-
+func posterButton[T any](title string, subTitle schwifty.Widgetable[T], image any) schwifty.Button {
 	return Button().
 		Child(
 			VStack(
